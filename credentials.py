4f1a3b73dc18dc23218bf35bf44316b467b2c091
javaris <578781015@qq.com>
2021-08-23 23:44:47 +0800

    first commit

---
+++ credentials.py
@@ -0,0 +1,3 @@
+bot_token = "1838523419:AAEh2n86CphWIAKFv2StcwJTHViMrsgYYjo"
+bot_user_name = "javarisbot"
+URL = "https://javaris.herokuapp.com/"
---
+++ telebot/credentials.py
@@ -0,0 +1,3 @@
+bot_token = "1838523419:AAEh2n86CphWIAKFv2StcwJTHViMrsgYYjo"
+bot_user_name = "javarisbot"
+URL = "the heroku app link that we will create later"
